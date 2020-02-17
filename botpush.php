1233789b128a30fc67e14e298efdff4058e92e55
Bee112233 <61152831+Bee112233@users.noreply.github.com>
2020-02-17 21:59:14 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -8,7 +8,7 @@ require "vendor/autoload.php";
 
 $access_token = 'MO0zl4hl3Qo8uQyiE0bB6uwIvmFXetyr2RDu+B7CH89EgTIxfUx+Le7RNoG2x49IN+CNMK5HLHm1KzzzIFV0wmECgJSBvuF3bk2N9sJku7SFLAPvLRWAFOqa4YDWeehMLg7yNvDiBvz4F+t+7T8IGQdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = 'd70897e2f43c3699be558305d86910ff';
+$channelSecret = 'd43dc42dc53fe096a0cfba02471ba49c';
 
 $pushID = 'Udeceabcc396d09173b50fe4c959f2809';
 
